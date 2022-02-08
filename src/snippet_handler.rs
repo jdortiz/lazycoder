5654ed4c7636e5a78e3e407b02c370af92f379f1
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-02-08 13:49:10 +0100

    Remove unnecessary file reading

--- src/snippet_handler.rs
+++ src/snippet_handler.rs
@@ -4,20 +4,13 @@ use crate::lazy_coder_error::LazyCoderError;
 
 pub struct SnippetHandler {
     filename: String,
-    count: usize,
 }
 
 impl SnippetHandler {
     pub fn new(path: &Path) -> Result<SnippetHandler, LazyCoderError> {
         if path.exists() {
-            let snippets: Vec<_> = fs::read_to_string(path)?
-                .split("\n---\n\n")
-                .map(|s| s.to_owned())
-                .collect();
-            eprintln!("Read {} from {}", snippets.len(), path.display());
             Ok(SnippetHandler {
                 filename: path.to_str().unwrap().to_owned(),
-                count: snippets.len(),
             })
         } else {
             Err(LazyCoderError::ConfigError {})
