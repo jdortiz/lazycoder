1bade37a708d5394802959e0ea9601ac9f0f165a
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2023-08-10 09:28:52 +0200

    More updates for clap v4
    - help_template
    - use rustdoc strings as help info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+//! Lazycoder - A simple snippet generator for expanso
+//!
+//! lazycoder start /filepath/demo.md
+//! - works with only one demo at a time
+//! - save initial next position: 0
+//! - file name
+//! - config. saved in ~/.lazycoder
+//! lazycoder next
+//! - reads from config file
+//! - reads next snippet
+//! - incs pointer to next snippet
+//! lazycoder rewind [number]
+//! - decs pointer (number times)
+//! - returns nothing
+//! lazycoder forward [number]
+//! - inc pointer (number times)
+//! - returns nothing
 mod config;
 mod lazy_coder_error;
 mod snippet_handler;
@@ -6,23 +23,13 @@ use clap::{Parser, Subcommand};
 use config::Config;
 use std::process::exit;
 
-// lazycoder start /filepath/demo.md
-// - works with only one demo at a time
-// - save initial next position: 0
-// - file name
-// - config. saved in ~/.lazycoder
-// lazycoder next
-// - reads from config file
-// - reads next snippet
-// - incs pointer to next snippet
-// lazycoder rewind [number]
-// - decs pointer (number times)
-// - returns nothing
-// lazycoder forward [number]
-// - inc pointer (number times)
-// - returns nothing
 #[derive(Parser)]
-#[clap(author,version,about,long_about=None)]
+#[command(author, version, about, long_about = None)]
+#[command(help_template = "{before-help}{name} {version}
+{author-with-newline}{about-section}
+{usage-heading} {usage}
+
+{all-args}{after-help}")] // This is required to show the author
 struct CliArgs {
     /// Verbosity level
     #[clap(short, long, action = clap::ArgAction::Count)]
@@ -33,21 +40,21 @@ struct CliArgs {
 
 #[derive(Subcommand)]
 enum Command {
-    #[clap(about = "Use <FILENAME> to provide snippets")]
+    /// Use <FILENAME> to provide snippets
     Start {
-        #[clap(help = "Path to snippet file")]
+        /// Path to snippet file
         filename: String,
     },
-    #[clap(about = "Provide next snippet")]
+    /// Provide next snippet
     Next {},
-    #[clap(about = "Rewind [n] snippet(s)")]
+    /// Rewind [n] snippet(s)
     Rewind {
-        #[clap(help = "n")]
+        /// Set n (by default is 1)
         count: Option<usize>,
     },
-    #[clap(about = "Forward [n] snippet(s)")]
+    /// Forward [n] snippet(s)
     Forward {
-        #[clap(help = "n")]
+        /// Set n (by default is 1)
         count: Option<usize>,
     },
 }
