5f45a2a70ae09913ca789fb7885e7697f0fbcef5
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2024-11-10 00:17:38 +0100

    Make Config::save testable and test it

--- src/config.rs
+++ src/config.rs
@@ -2,20 +2,19 @@ use crate::{
     lazy_coder_error::LazyCoderError,
     snippet_handler::{SnippetHandler, SnippetProvider},
 };
-use directories::ProjectDirs;
 use log::{debug, error};
 use serde_derive::{Deserialize, Serialize};
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
 #[cfg(not(test))]
-use aux::{canonicalize, config_dir};
+use aux::{config_dir, get_snippet_provider, path_exists};
 #[cfg(not(test))]
-use std::fs::read_to_string;
+use std::fs::{canonicalize, create_dir_all, read_to_string, write};
 #[cfg(test)]
-use tests::aux::{canonicalize, config_dir, read_to_string};
+use tests::aux::{
+    canonicalize, config_dir, create_dir_all, get_snippet_provider, path_exists, read_to_string,
+    write,
+};
 
 static FILE_NAME: &str = "lazycoder.toml";
 
@@ -72,8 +71,8 @@ impl Config {
     ///
     pub fn next(&mut self) -> Result<String, LazyCoderError> {
         let path = PathBuf::from(self.file_path.clone());
-        let snippet_hdlr: SnippetHandler = SnippetHandler::new(&path)?;
-        let snippet = snippet_hdlr.get_snippet(self.position)?;
+        let snippet_prov = get_snippet_provider(&path)?;
+        let snippet = snippet_prov.get_snippet(self.position)?;
         self.position += 1;
         self.save(false)?;
         Ok(snippet)
@@ -100,22 +99,23 @@ impl Config {
         }
     }
 
+    /// Saves this configuration to the standard path and file.
     fn save(&self, create_dir: bool) -> Result<(), LazyCoderError> {
         let toml_text = toml::to_string(&self).expect("Failing to encode TOML");
-        if let Some(project_dirs) = ProjectDirs::from("com", "mongodb", "lazycoder") {
-            if create_dir {
-                let config_dir = project_dirs.config_dir();
-                if !config_dir.exists() {
-                    fs::create_dir_all(config_dir)?;
+        if let Some(mut config_path) = config_dir() {
+            if !path_exists(&config_path) {
+                if create_dir {
+                    create_dir_all(config_path.clone())?;
+                } else {
+                    return Err(LazyCoderError::ConfigDirError);
                 }
             }
-            let mut config_file = project_dirs.config_dir().to_path_buf();
-            config_file.push(FILE_NAME);
+            config_path.push(FILE_NAME);
             debug!(
                 "Writing configuration to file {}",
-                config_file.as_path().display()
+                config_path.as_path().display()
             );
-            fs::write(config_file, toml_text)?;
+            write(config_path, toml_text)?;
             Ok(())
         } else {
             Err(LazyCoderError::ConfigDirError)
@@ -125,16 +125,14 @@ impl Config {
 
 #[cfg(not(test))]
 mod aux {
-    use std::{
-        fs, io,
-        path::{Path, PathBuf},
-    };
+    use std::path::{Path, PathBuf};
 
     use directories::ProjectDirs;
 
-    pub fn canonicalize<P: AsRef<Path>>(path: P) -> io::Result<PathBuf> {
-        fs::canonicalize(path)
-    }
+    use crate::{
+        lazy_coder_error::LazyCoderError,
+        snippet_handler::{SnippetHandler, SnippetProvider},
+    };
 
     pub(crate) fn config_dir() -> Option<PathBuf> {
         Some(
@@ -142,6 +140,14 @@ mod aux {
                 .config_dir()
                 .to_path_buf(),
         )
+    }
+
+    pub fn get_snippet_provider(path: &Path) -> Result<Box<dyn SnippetProvider>, LazyCoderError> {
+        Ok(Box::new(SnippetHandler::new(path)?))
+    }
+
+    pub fn path_exists<P: AsRef<Path>>(path: P) -> bool {
+        path.as_ref().exists()
     }
 }
 
@@ -152,9 +158,16 @@ mod tests {
     use super::*;
 
     thread_local! {
-    static CANONIZALIZE_ANSWER: Cell<Option<PathBuf>> = Cell::new(None);
-    static CONFIG_DIR_ANSWER: Cell<Option<PathBuf>> = Cell::new(None);
-    static READ_TO_STRING_ANSWER: Cell<Option<String>> = Cell::new(None);
+        static CANONIZALIZE_ANSWER: Cell<Option<PathBuf>> = Cell::new(None);
+        static CONFIG_DIR_ANSWER: Cell<Option<PathBuf>> = Cell::new(None);
+        static CREATE_DIR_OK_ANSWER: Cell<bool> = Cell::new(true);
+        static CREATE_DIR_ALL_ARG: Cell<Option<PathBuf>> = Cell::new(None);
+        static PATH_EXISTS_ANSWER: Cell<bool> = Cell::new(true);
+        static READ_TO_STRING_ANSWER: Cell<Option<String>> = Cell::new(None);
+        static SNIPPET_PROVIDER_ANSWER: Cell<Option<Box<dyn SnippetProvider>>> = Cell::new(None);
+        static WRITE_OK_ANSWER: Cell<bool> = Cell::new(true);
+        static WRITE_ARG_PATH: Cell<Option<PathBuf>> = Cell::new(None);
+        static WRITE_ARG_CONTENTS: Cell<Option<String>> = Cell::new(None);
     }
 
     #[test]
@@ -171,10 +184,14 @@ mod tests {
         let mut path = PathBuf::new();
         path.push("/tmp");
         CANONIZALIZE_ANSWER.set(Some(path));
+        CONFIG_DIR_ANSWER.set(Some(PathBuf::from_str("Some path").unwrap()));
 
         let sut = Config::new(Path::new("/tmp"));
 
-        assert!(matches!(sut, Ok(cfg) if cfg.file_path == "/tmp" && cfg.position == 0));
+        assert!(
+            matches!(sut, Ok(ref cfg) if cfg.file_path == "/tmp" && cfg.position == 0),
+            "Wrong config: {sut:?}"
+        );
     }
 
     #[test]
@@ -212,13 +229,87 @@ mod tests {
         ));
     }
 
+    #[test]
+    fn save_returns_error_if_config_dir_fails() {
+        CONFIG_DIR_ANSWER.set(None);
+        let sut = Config {
+            file_path: String::from("/some/config/path"),
+            position: 3,
+        };
+
+        assert!(matches!(
+            sut.save(true),
+            Err(LazyCoderError::ConfigDirError)
+        ));
+    }
+
+    #[test]
+    fn save_creates_dir_if_doesnt_exist_and_rnquested() {
+        let path_buf = PathBuf::from("/some/config/path");
+        CONFIG_DIR_ANSWER.set(Some(path_buf.clone()));
+        PATH_EXISTS_ANSWER.set(false);
+        let sut = Config {
+            file_path: String::from("/some/config/path"),
+            position: 3,
+        };
+
+        assert!(matches!(sut.save(true), Ok(())));
+
+        assert_eq!(CREATE_DIR_ALL_ARG.take(), Some(path_buf));
+    }
+
+    #[test]
+    fn save_returns_error_if_doesnt_config_dir_doesnt_exist_and_create_dir_is_false() {
+        let path_buf = PathBuf::from("/some/config/path");
+        CONFIG_DIR_ANSWER.set(Some(path_buf.clone()));
+        PATH_EXISTS_ANSWER.set(false);
+        let sut = Config {
+            file_path: String::from("/some/config/path"),
+            position: 3,
+        };
+
+        assert!(matches!(
+            sut.save(false),
+            Err(LazyCoderError::ConfigDirError)
+        ));
+    }
+
+    #[test]
+    fn save_stores_configuration_in_stanard_file() {
+        let mut path_buf = PathBuf::from("/some/config/path");
+        CONFIG_DIR_ANSWER.set(Some(path_buf.clone()));
+        PATH_EXISTS_ANSWER.set(true);
+        let sut = Config {
+            file_path: String::from("/some/config/path"),
+            position: 3,
+        };
+
+        // TODO: path.exists() should also be mocked. and then create_dir_all should be a spy.
+        assert!(matches!(sut.save(true), Ok(())));
+
+        path_buf.push(FILE_NAME);
+        assert_eq!(WRITE_ARG_PATH.take(), Some(path_buf));
+        assert_eq!(
+            WRITE_ARG_CONTENTS.take(),
+            Some(String::from(
+                "file_path = \"/some/config/path\"\nposition = 3\n"
+            ))
+        );
+    }
+
     pub mod aux {
         use std::{
             io,
             path::{Path, PathBuf},
         };
 
-        use super::{CANONIZALIZE_ANSWER, CONFIG_DIR_ANSWER, READ_TO_STRING_ANSWER};
+        use crate::{lazy_coder_error::LazyCoderError, snippet_handler::SnippetProvider};
+
+        use super::{
+            CANONIZALIZE_ANSWER, CONFIG_DIR_ANSWER, CREATE_DIR_ALL_ARG, CREATE_DIR_OK_ANSWER,
+            PATH_EXISTS_ANSWER, READ_TO_STRING_ANSWER, SNIPPET_PROVIDER_ANSWER, WRITE_ARG_CONTENTS,
+            WRITE_ARG_PATH, WRITE_OK_ANSWER,
+        };
 
         pub fn canonicalize<P: AsRef<Path>>(_path: P) -> io::Result<PathBuf> {
             match CANONIZALIZE_ANSWER.take() {
@@ -231,10 +322,47 @@ mod tests {
             CONFIG_DIR_ANSWER.take()
         }
 
+        pub fn create_dir_all<P: AsRef<Path>>(path: P) -> io::Result<()> {
+            CREATE_DIR_ALL_ARG.set(Some(path.as_ref().to_path_buf()));
+            if CREATE_DIR_OK_ANSWER.get() {
+                Ok(())
+            } else {
+                Err(io::Error::other("Some error"))
+            }
+        }
+
+        pub fn get_snippet_provider(
+            _path: &Path,
+        ) -> Result<Box<dyn SnippetProvider>, LazyCoderError> {
+            match SNIPPET_PROVIDER_ANSWER.take() {
+                Some(snippet_prov) => Ok(snippet_prov),
+                None => Err(LazyCoderError::RunOutOfSnippets),
+            }
+        }
+
+        pub fn path_exists<P: AsRef<Path>>(_path: P) -> bool {
+            PATH_EXISTS_ANSWER.get()
+        }
+
         pub(crate) fn read_to_string<P: AsRef<Path>>(_path: P) -> std::io::Result<String> {
             match READ_TO_STRING_ANSWER.take() {
                 Some(lines) => Ok(lines),
                 None => Err(io::Error::other("Some error")),
+            }
+        }
+
+        pub(crate) fn write<P: AsRef<Path>, C: AsRef<[u8]>>(
+            path: P,
+            contents: C,
+        ) -> io::Result<()> {
+            WRITE_ARG_PATH.set(Some(path.as_ref().to_path_buf()));
+            if let Ok(cont_str) = std::str::from_utf8(contents.as_ref()) {
+                WRITE_ARG_CONTENTS.set(Some(cont_str.to_string()));
+            }
+            if WRITE_OK_ANSWER.get() {
+                Ok(())
+            } else {
+                Err(io::Error::other("Some error"))
             }
         }
     }
