8c3b69a35037c9e9eaddbb2bdbf7ca93e177e540
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2024-11-20 09:54:08 +0100

    Add tests for the LazyCoderErrors

--- src/lazy_coder_error.rs
+++ src/lazy_coder_error.rs
@@ -52,3 +52,85 @@ impl From<toml::de::Error> for LazyCoderError {
         LazyCoderError::ConfigEncoding(err)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use serde::de::Error;
+
+    #[test]
+    fn display_snippet_file_not_found_error() {
+        assert_eq!(
+            LazyCoderError::SnippetFileNotFound.to_string(),
+            "snippet file not found"
+        )
+    }
+
+    #[test]
+    fn display_snippet_file_error() {
+        assert_eq!(
+            LazyCoderError::SnippetFileError(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "some file error"
+            ))
+            .to_string(),
+            "snippet file error: some file error"
+        )
+    }
+
+    #[test]
+    fn display_run_out_of_snippets_error() {
+        assert_eq!(
+            LazyCoderError::RunOutOfSnippets.to_string(),
+            "out of range of available snippets"
+        )
+    }
+
+    #[test]
+    fn display_config_dir_error() {
+        assert_eq!(
+            LazyCoderError::ConfigDirError.to_string(),
+            "no valid home directory path could be retrieved"
+        )
+    }
+
+    #[test]
+    fn display_config_file_error() {
+        assert_eq!(
+            LazyCoderError::ConfigFileError(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "some file error"
+            ))
+            .to_string(),
+            "configuration file error: some file error"
+        )
+    }
+
+    #[test]
+    fn display_config_encoding_error() {
+        assert_eq!(
+            LazyCoderError::ConfigEncoding(toml::de::Error::custom("some file error")).to_string(),
+            "configuration encoding error: some file error\n" // Newline added by toml::de::Error
+        )
+    }
+
+    #[test]
+    fn display_operation_out_of_range_error() {
+        assert_eq!(
+            LazyCoderError::OperationOutOfRange.to_string(),
+            "operation out of range"
+        )
+    }
+
+    #[test]
+    fn toml_error_conversion() {
+        let toml_error = toml::de::Error::custom("some file error");
+        let new_error = LazyCoderError::from(toml_error.clone());
+
+        assert!(matches!(
+            new_error,
+            LazyCoderError::ConfigEncoding(err) if err == toml_error
+        ));
+    }
+}
